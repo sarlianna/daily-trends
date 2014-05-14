ea384d9d23b4152ba7c7a8825dd7e4caeb3fbde2
Zekna <zsharpsquared@gmail.com>
2014-05-15 08:17:10 +0900

    Working client-side code.

--- client/app/project.clj
+++ client/app/project.clj
@@ -1,21 +1,26 @@
 (defproject app "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"] 
                  [org.clojure/clojurescript "0.0-2202"]
-                 [om "0.5.0"]]
+                 [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
+                 [org.clojure/data.json "0.2.4"]
+                 [org.clojure/tools.trace "0.7.8"]
+                 [om "0.5.0"]
+                 [org.clojure/data.json "0.2.4"]
+                 [ring "1.2.2"]
+                 [garden "1.1.6"]
+                 [compojure "1.1.8"]
+                 [sablono "0.2.16"]]
 
-  :main ^:skip-aot app.core
-  :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}}
-  :plugins [[lein-cljsbuild "1.0.3"]]
-  :cljsbuild {
-               :builds [{
-                         :source-paths ["src-cljs"]
-                         :compiler {
-                                    :output-to "war/javascripts/main.js"
-                                    :optimizations :whitespace
-                                    :pretty-print true}}]}
-            )
+  :plugins [[lein-cljsbuild "1.0.3"]
+            [lein-ring "0.8.10"]]
+  :source-paths ["src/app" "src/server"]
+  :ring {:handler server/app}
+  :cljsbuild { :builds [{:id "dev"
+                         :source-paths ["src/app"]
+                         :compiler {:externs ["react/react-with-addons.min.js"]
+                                    :closure-warnings {:externs-validation :off
+                                                       :non-standard-jsdoc :off}
+                                    :output-to "./resources/public/dist/app.js"
+                                    :output-dir "./resources/public/dist/out"
+                                    :optimizations :none
+                                    :source-map true}}]})
---
+++ client/app/src/app/site/home.cljs
@@ -0,0 +1,7 @@
+(ns app.site.home
+  (:require [om.core :as om :include-macros true]
+            [sablono.core :as html :refer-macros [html]]))
+
+(enable-console-print!)
+
+
